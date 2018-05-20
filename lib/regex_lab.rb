4731b0311afd779a3c72d413c99a03fa69a433d2
Mohammed Noori <kmanoorib@gmail.com>
2018-05-20 05:02:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,9 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\d{3,4}/)
-    true
-  else
+  if phone.match(/\D/)
     false
+  else
+    true
   end
 end
