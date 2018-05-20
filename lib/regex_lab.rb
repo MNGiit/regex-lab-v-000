7e08c57c861bf8435a5a9e69dbb6ad1ace31b5ef
Mohammed Noori <kmanoorib@gmail.com>
2018-05-20 05:25:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\D/)
+  if phone.match(/\[0-9]{11}/)
     false
   elsif phone.match(/\[0-9]{3,4}/)
     true
